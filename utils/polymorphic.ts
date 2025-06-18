4c261688bd5800dd29407682a098944fdf07b764
Jeremi Wielewski <jerwier420@gmail.com>
2025-06-18 11:08:35 +0200

    added polymporphic component props

---
+++ utils/polymorphic.ts
@@ -0,0 +1,14 @@
+import { ElementType } from "react";
+
+type AsProp<C extends ElementType> = {
+  as?: C;
+};
+
+type PropsToOmit<C extends React.ElementType, P> = keyof (AsProp<C> & P);
+
+export type PolymorphicComponentProps<
+  C extends React.ElementType,
+  Props = {}
+> = React.PropsWithChildren<Props & AsProp<C>> &
+  Omit<React.ComponentPropsWithoutRef<C>,
+    PropsToOmit<C, Props>>;
